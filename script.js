ad652e184cf43b6456624c9e3067c69c0e05d178
Ryan Harris <harris.ucla@gmail.com>
2020-03-18 22:20:16 -0700

    update time block colors every minute

--- script.js
+++ script.js
@@ -3,27 +3,31 @@ $(init);
 function init() {
   // get current day and display on top of page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
-  // color our time blocks
-  $(".time-block").each(updateTimeBlock);
+  // color our time blocks and update every minute in case the hour changes
+  updateTimeBlocks();
+  setInterval(updateTimeBlocks, 60000);
   // attach our handler for the save buttons
   $(".saveBtn").on("click", handleSave);
 }
 
-function updateTimeBlock() {
-  var blockId = $(this).attr("id");
-  var blockHour = parseInt(blockId.replace("hour-", ""));
-  var currentHour = parseInt(moment().format("H"));
-  // color block based on past, present, future class
-  if (blockHour < currentHour) {
-    $(this).addClass("past");
-  } else if (blockHour > currentHour) {
-    $(this).addClass("future");
-  } else {
-    $(this).addClass("present");
-  }
-
-  // load saved data from local storage
-  $("#" + blockId + " > textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+function updateTimeBlocks() {
+  // for each time block
+  $(".time-block").each(function() {
+    var blockId = $(this).attr("id");
+    var blockHour = parseInt(blockId.replace("hour-", ""));
+    var currentHour = parseInt(moment().format("H"));
+    // color block based on past, present, future class
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  
+    // load saved data from local storage
+    $("#" + blockId + " > textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+  });
 }
 
 function handleSave(event) {
